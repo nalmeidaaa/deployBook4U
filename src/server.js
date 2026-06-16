af299e97daaf3c8fb4abba70f95d0875030fea76
NICOLAS ALMEIDA BARBOSA <nicolas.barbosa19@senaisp.edu.br>
2026-06-16 15:54:21 -0300

    correção server 2

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from "express";
 import routes from "./routes/routes.js";
+import { initializeDatabase } from "./configs/Database.js";
 import cors from 'cors';
 
 const app = express();
