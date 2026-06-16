093f4dd4572621c7482f0b8788e7bd23d40c38ff
NICOLAS ALMEIDA BARBOSA <nicolas.barbosa19@senaisp.edu.br>
2026-06-16 15:49:36 -0300

    adicao imagens

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import routes from './routes/routes.js';
 import { initializeDatabase } from './configs/Database.js';
-
+import cors from 'cors';
 
 const app = express();
 
@@ -10,6 +10,8 @@ const app = express();
 app.use(express.json());
 app.use('/', routes);
 
+app.use('/uploads', express.static('uploads'));
+app.use('/', routes);
 
 initializeDatabase().then(() => {
     app.listen(process.env.SERVER_PORT, () => {
@@ -18,3 +20,5 @@ initializeDatabase().then(() => {
 }).catch(err => {
     console.error("Erro ao inicializar o banco de dados:", err);
 });
+
+
