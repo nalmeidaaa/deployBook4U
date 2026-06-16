147ba2da367ecf63f199e739140a7ec9ac82085b
NICOLAS ALMEIDA BARBOSA <nicolas.barbosa19@senaisp.edu.br>
2026-06-16 15:57:54 -0300

    correção server 3

--- src/server.js
+++ src/server.js
@@ -7,7 +7,6 @@ import cors from 'cors';
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/', routes);
 
 app.use('/uploads', express.static('uploads'));
 app.use('/', routes);
