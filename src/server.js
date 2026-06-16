7e2e95d4d327777002dd50976af13a9e938febb4
NICOLAS ALMEIDA BARBOSA <nicolas.barbosa19@senaisp.edu.br>
2026-06-16 13:01:58 -0300

    7 commit

--- src/server.js
+++ src/server.js
@@ -1,15 +1,20 @@
 import 'dotenv/config';
-import express from "express";
-import routes from "./routes/routes.js";
-import cors from 'cors';
+import express from 'express';
+import routes from './routes/routes.js';
+import { initializeDatabase } from './configs/Database.js';
+
 
 const app = express();
-app.use(cors());
-app.use(express.json());
 
-app.use('/uploads', express.static('uploads'));
+
+app.use(express.json());
 app.use('/', routes);
 
-app.listen(process.env.SERVER_PORT, () => {
-    console.log(`Servidor rodando em http://${process.env.DB_HOST}:${process.env.SERVER_PORT}`);
+
+initializeDatabase().then(() => {
+    app.listen(process.env.SERVER_PORT, () => {
+        console.log(`Servidor rodando na porta ${process.env.SERVER_PORT}`);
+    });
+}).catch(err => {
+    console.error("Erro ao inicializar o banco de dados:", err);
 });
